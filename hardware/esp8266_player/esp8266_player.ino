6c8ea7d122095380c1257cfa1b57116505a215e6
Petr Popov <popov.petr@gmail.com>
2017-05-03 23:56:41 +0300

    working

--- hardware/esp8266_player/esp8266_player.ino
+++ hardware/esp8266_player/esp8266_player.ino
@@ -1,19 +1,31 @@
 #include "EEPROMParams.h"
+#include <aJSON.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
 #include <OneButton.h>
 #include <DFPlayer_Mini_Mp3.h>
 #include <SoftwareSerial.h>
 #include <WiFiManager.h>
+
+#define HTTP_DEBUG
 
 #define MAGIC_ADDR 0
 #define MAGIC_VALUE 42
 #define RX 13
 #define TX 12
 #define BADUM_PIN 14
-#define FAIL_PIN 16
+#define FAIL_PIN 5
 #define SOUND_LEVEL_PIN A0
 
 #define BADUM_NUMBER 1
 #define FAIL_NUMBER 2
+
+const String apiHost = "http://51.15.69.129";
+uint32_t chipId;
+const String chipName = "Moscow";
+const String apiSecret = "asaf3rfalf3k23fmaskfj3";
+const String badumPath = "/api/badum";
+const String failPath = "/api/fail";
 
 //this is for controlling volume
 int oldVolume = 0;
@@ -50,6 +62,8 @@ void setup() {
   //read counters params
   paramsWriter->readSoundParams(params);
 
+  chipId = ESP.getChipId();
+
   //setup Serial
   Serial.begin(115200);
   Serial.println("Starting");
@@ -70,7 +84,57 @@ void loop() {
   badumButton.tick();
   failButton.tick();
   processSoundLevel();
+  checkConfigCounter();
+}
 
+//rest api
+const char* getBodyForCounter(unsigned long counter) {
+  aJsonObject *body = aJson.createObject();
+  aJson.addStringToObject(body, "counter", String(counter).c_str());
+  aJson.addStringToObject(body, "chipId", String(chipId).c_str());
+  aJson.addStringToObject(body, "chipName", chipName.c_str());
+  aJson.addStringToObject(body, "apisecret", apiSecret.c_str());
+  return aJson.print(body);
+}
+
+const String getBadumApiUrl() {
+  return apiHost + badumPath;
+}
+
+const String getFailApiUrl() {
+  return apiHost + failPath;
+}
+
+void postMethod(unsigned long counter, const String host) {
+  HTTPClient http;
+  http.begin(host);
+  http.addHeader("Content-Type", "application/json"); 
+
+  const char *payload = getBodyForCounter(counter);
+  int httpCode = http.POST((uint8_t *)payload, strlen(payload));
+
+  String answer = http.getString();                  //Get the response payload
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(answer);    //Print request response payload
+  http.end(); 
+}
+
+void sendRestBadum(unsigned long counter) {
+  Serial.print("Posting to /api/badum counter ");
+  Serial.println(counter);
+  
+  postMethod(counter, getBadumApiUrl());
+}
+
+void sendRestFail(unsigned long counter) {
+  Serial.print("Posting to /api/fail counter ");
+  Serial.println(counter);
+  
+  postMethod(counter, getFailApiUrl());
+}
+
+//callbacks
+void checkConfigCounter() {
   long nowTime = millis();
   if(badumLongPress && badumPressInitTime != 0 && nowTime - badumPressInitTime >= configLimitTime) {
     stopConfigCounter();
@@ -78,7 +142,6 @@ void loop() {
   delay(10);
 }
 
-//callbacks
 void startConfigCounter() {
   if(badumLongPress == true) {
     Serial.println("Already started configuration init mode");
@@ -142,14 +205,17 @@ void processSoundLevel() {
   }
 }
 
-void playBadum() {
+void playBadum() {  
   long nowTime = millis();
   if(nowTime - lastPlayStartTime > deltaTime) {
     lastPlayStartTime = nowTime;
     params->badumCounter++;
-    Serial.println(params->badumCounter);
     paramsWriter->writeSoundParams(params);
     playSound(BADUM_NUMBER); 
+
+    if(WiFi.status() == WL_CONNECTED) {
+      sendRestBadum(params->badumCounter);
+    }
   }
 }
 
@@ -160,6 +226,10 @@ void playFail() {
     params->failCounter++;
     paramsWriter->writeSoundParams(params);
     playSound(FAIL_NUMBER);
+
+    if(WiFi.status() == WL_CONNECTED) {
+      sendRestFail(params->failCounter);
+    }
   }
 }
 
